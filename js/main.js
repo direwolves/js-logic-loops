445eea7e62ed733fe575d7a199550b978c9a6550
Sean Xavier <seanxhallion@gmail.com>
2016-12-08 18:54:59 -0500

    beginning js-logic-loops

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// string
+var hackers = 'ZeroCool';
+// number
+var crashed = 1507;
+// boolean
+var hackThePlanet = true;
+
+// array
+var helpers = ['CerealKiller', 'LordNikon', 'AcidBurn'];
+
+// object
+var joey = {
+	firstTry: 'UltraLazer',
+	secondTry: 'DoctorDoom',
+	thirdTry: 'MasterofDisaster',
+	cast: helpers
+};
+
+console.log(hackers)
+console.log(joey)
+console.log(helpers)
+console.log(crashed)
+console.log(hackThePlanet)
+console.log(joey.cast[2])
